557f594c62c0cef66862831fb6e37e2ab0f98dc0
José Luis Terán <joseluisteran.study@gmail.com>
2024-09-08 17:37:46 -0400

    chore: update app-root name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'concell-system-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
