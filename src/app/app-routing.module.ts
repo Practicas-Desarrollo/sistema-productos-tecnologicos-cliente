7ab66b133c87164233d578ed945f5a26fd0c7b09
José Luis Terán <joseluisteran.study@gmail.com>
2024-09-10 14:29:32 -0400

    feat: implement route for product list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
+import { ProductListComponent } from './features/product/product-list/product-list.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'products', component: ProductListComponent }
 ];
 
 @NgModule({
